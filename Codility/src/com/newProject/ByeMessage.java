c04ec465f78b1fecf518df4cd5044a6f5ebe6e46
Rishabh Singh <rishabh.singh@lntinfotech.com>
2020-12-18 10:21:32 +0530

    Added 2-3 lines in ByeMessage class

--- Codility/src/com/newProject/ByeMessage.java
+++ Codility/src/com/newProject/ByeMessage.java
@@ -4,6 +4,10 @@ public class ByeMessage {
 
 	public static void main(String[] args) {
 		System.out.println("Bye world");
+		System.out.println("End");
+		System.out.println("...");
+		System.out.println("...");
+		
 	}
 
 }
